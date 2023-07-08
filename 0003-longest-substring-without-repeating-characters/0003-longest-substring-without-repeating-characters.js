c058eac1d26922a1a6a6e51bacd7838613eb4e62
Andre Fajar N <62334317+andre-fajar-n@users.noreply.github.com>
2023-07-08 11:53:25 +0700

    Time: 97 ms (45.07%), Space: 50.6 MB (12.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -8,18 +8,14 @@ var lengthOfLongestSubstring = function(s) {
     for (const idx in s) {
         const char = s[idx]
         if (substring.includes(char)) {
-            console.log("CURR", idx, char)
             if (substring.length > output) {
                 output = substring.length
             }
-            console.log("SUB", substring.slice(substring.indexOf(char)+1))
-            console.log("THIS", substring.indexOf(char))
             substring = substring.slice(substring.indexOf(char)+1)
         }
         substring += char
     }
 
-    console.log("CEK", substring)
     if (substring.length > output) {
         output = substring.length
     }
