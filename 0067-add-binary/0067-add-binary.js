c4f5b4fe3e4d9971751b9d68bdceb4c291eace26
Andre Fajar N <62334317+andre-fajar-n@users.noreply.github.com>
2023-07-09 09:17:22 +0700

    Time: 68 ms (44.86%), Space: 45.1 MB (13.32%) - LeetHub

--- 0067-add-binary/0067-add-binary.js
+++ 0067-add-binary/0067-add-binary.js
@@ -10,24 +10,19 @@ var addBinary = function(a, b) {
     let output = ""
 
     while (i >= 0 || j >= 0 || carry == 1) {
-        console.log("AWAL", output, "CARRY", carry)
         if (i >= 0) {
             carry += parseInt(a[i])
-            console.log("A", a[i], carry)
             i--
         }
 
         if (j >= 0) {
             carry += parseInt(b[j])
-            console.log("B", b[j], carry)
             j--
         }
 
         const temp = carry % 2
-        console.log("TEMP", temp, carry, output)
         output = temp.toString() + output
         carry = parseInt(carry/2)
-        console.log("AKHIR", output, "CARRY", carry)
     }
 
     return output
