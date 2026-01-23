d140ab26d2b2f478a3ff56a307545ba3b989a221
Andre Fajar N <62334317+andre-fajar-n@users.noreply.github.com>
2026-01-23 10:45:38 +0700

    Time: 56 ms (0%), Space: 7 MB (9.09%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.go
@@ -0,0 +1,42 @@
+func minimumPairRemoval(nums []int) int {
+    output:=0
+
+    for len(nums) > 1 {
+        fmt.Println("nums.   ",nums)
+        new_nums, already_sorted := decreaseMinimumPair(nums)
+        fmt.Println("new_nums",new_nums)
+        fmt.Println("already_sorted",already_sorted)
+
+        if already_sorted {
+            break
+        }
+        output++
+        nums = new_nums
+    }
+
+    return output
+}
+
+func decreaseMinimumPair(nums []int) ([]int, bool) {
+    already_sorted := true
+    minimum_sum_adjacent := math.MaxInt
+    index_first_pair_adjacent := -1
+    for i:=0; i<len(nums) - 1; i++ {
+        minimum_sum := nums[i] + nums[i+1]
+        if nums[i] > nums[i+1] {
+            already_sorted = false
+        }
+        if minimum_sum < minimum_sum_adjacent {
+            minimum_sum_adjacent = minimum_sum
+            index_first_pair_adjacent = i
+        }
+    }
+
+    if already_sorted {
+        return nums, already_sorted
+    }
+    new_nums := append(nums[:index_first_pair_adjacent], minimum_sum_adjacent)
+    new_nums = append(new_nums, nums[index_first_pair_adjacent+2:]...)
+
+    return new_nums, already_sorted
+}
